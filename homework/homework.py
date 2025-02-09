81e795cc88de1d2cc76e84cda351df8cae36d342
Oscar Pérez <operezq@unal.edu.co>
2025-02-09 18:15:27 -0500

    Laboratorio completo

--- homework/homework.py
+++ homework/homework.py
@@ -4,6 +4,10 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import zipfile
+import glob
+import os
 
 def clean_campaign_data():
     """
@@ -49,9 +53,35 @@ def clean_campaign_data():
 
 
     """
+    datas = []
 
-    return
+    for directorio in os.listdir("files/input"):
+        with zipfile.ZipFile(os.path.join("files/input",directorio), 'r') as archivo:
+            for csv in archivo.namelist():
+                with archivo.open(csv) as file:
+                    df = pd.read_csv(file, sep=",", encoding="utf-8")
+                    datas.append(df)
 
+    df = pd.concat(datas, ignore_index=True)
 
+    cliente = df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
+    cliente['job'] = cliente['job'].str.replace('.', '').str.replace('-', '_')
+    cliente['education'] = cliente['education'].str.replace('.', '_').replace('unknown', pd.NA)
+    cliente['credit_default'] = cliente['credit_default'].map({'yes': 1}).fillna(0)
+    cliente['mortgage'] = cliente['mortgage'].map({'yes': 1}).fillna(0)
+    cliente.to_csv('files/output/client.csv',index=False)
+
+    campaign = df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome', 'day', 'month']].copy()
+    campaign['previous_outcome'] = campaign['previous_outcome'].apply(lambda x: 1 if x == "success" else 0)
+    campaign['campaign_outcome'] = campaign['campaign_outcome'].apply(lambda x: 1 if x == "yes" else 0)
+    campaign['last_contact_date'] = pd.to_datetime('2022-' + campaign['month'] + '-' + campaign['day'].astype(str), format='%Y-%b-%d').dt.strftime('%Y-%m-%d')
+    campaign.drop(columns=['month', 'day'], inplace=True)
+    campaign.to_csv('files/output/campaign.csv',index=False)
+
+    economics = df[['client_id','cons_price_idx','euribor_three_months']]
+    economics.to_csv('files/output/economics.csv',index=False)
+
+    print(df)
 if __name__ == "__main__":
     clean_campaign_data()
+    
